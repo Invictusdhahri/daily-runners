abb41e7de9f14ecce2d5c7b9ad4d7f06351c9496
Amen Dhahri🌙 <invictus.dhahri@gmail.com>
2025-05-04 22:37:14 +0100

    update replit

--- replit.nix
+++ replit.nix
@@ -1,17 +1,12 @@
 { pkgs }: {
   deps = [
     pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript
-    pkgs.yarn
-    pkgs.replitPackages.jest
     pkgs.libuuid
-    pkgs.libGL
     pkgs.cairo
     pkgs.pango
-    pkgs.pkg-config
     pkgs.libjpeg
     pkgs.giflib
     pkgs.librsvg
-    pkgs.pixman
+    pkgs.pkg-config
   ];
 } 
